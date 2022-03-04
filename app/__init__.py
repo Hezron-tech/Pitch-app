7dad215b6c1167b74cb6f04341db6571bef1b8d0
Hezron-tech <kiprophez@gmail.com>
2022-03-05 01:47:59 +0300

    creating the app function

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,25 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# Initializing application
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-from app import views
+    app.config['SECRET_KEY'] = 'secret-key-goes-here'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+    db.init_app(app)
+
+    # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+
+    
+
+    return app
 
