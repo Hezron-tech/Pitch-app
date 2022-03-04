dde6932d25ee9accaf8e02fd59b2267be17f4e7b
Hezron-tech <kiprophez@gmail.com>
2022-03-05 02:07:48 +0300

    adding class user

--- app/models.py
+++ app/models.py
@@ -4,8 +4,24 @@ from . import db
 
 
 
+from . import db
+
 class User(db.Model):
+
     __tablename__="users"
+
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(1000))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+
+
+
+
+
+    
 
     
 
