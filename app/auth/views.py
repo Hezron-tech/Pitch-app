2b8d7c9d58ad11edaaf6ce7e144e8c214cc75f4b
Hezron-tech <kiprophez@gmail.com>
2022-03-05 23:16:50 +0300

    creating auth views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,10 @@
-from flask import render_template
+from flask import render_template,redirect,url_for,flash,request
+from flask_login import login_user,logout_user,login_required
+from ..models import User
 from . import auth
+from .forms import LoginForm, RegistrationForm
+from .. import db
+# from ..email import mail_message
 
 @auth.route('/login')
 def login():
