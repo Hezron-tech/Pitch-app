ad90181d987e56e8f5959bdcbc6a76f75d6bd02a
Hezron-tech <kiprophez@gmail.com>
2022-03-05 22:15:45 +0300

    creating auth views

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-
+from werkzeug.security import generate_password_hash,check_password_hash
 
 from . import db
 
@@ -14,7 +14,22 @@ class User(db.Model):
     bio = db.Column(db.String(255))
     pitch = db.relationship("Pitch", backref="author", lazy="dynamic")
     profile_pic_path = db.Column(db.String())
+    pass_secure  = db.Column(db.String(255))
     password_hash = db.Column(db.String(255))
+
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User {self.username}'
