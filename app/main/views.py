46c6aefe7aa54da0195dab70269d4967bafddebb
Hezron-tech <kiprophez@gmail.com>
2022-03-05 23:09:58 +0300

    adding base.html

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ from flask import render_template,request,redirect,url_for
 from flask import render_template
 from app import app
 from ..models import User,Pitch
+from flask_login import login_required
 
 # Views
 @app.route('/')
