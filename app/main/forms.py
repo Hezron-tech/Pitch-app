74ba791ce7c2dc899c9972bc1440a34b969eaac6
Hezron-tech <kiprophez@gmail.com>
2022-03-06 21:56:58 +0300

    adding html

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,18 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField,SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import Required
 
 class PitchForm(FlaskForm):
-  title = StringField('Pitch title',validators=[DataRequired()])
-  category = SelectField("Choose Category",choices=[('sciences','sciences'),('tech','tech'),('politics','politics')])
-  pitch_info = TextAreaField('Your Pitch',validators=[DataRequired()])
+  title = StringField('Pitch title',validators=[Required()])
+  category = SelectField("Choose Category",choices=[('business','business'),('sports','sports'),('politics','politics')])
+  pitch_info = TextAreaField('Your Pitch',validators=[Required()])
   submit = SubmitField('Submit')
-
-  
 class CommentForm(FlaskForm):
-  comment = TextAreaField('Your Comment',validators=[DataRequired()])
+  comment = TextAreaField('Your Comment',validators=[Required()])
   submit = SubmitField('Submit')
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
