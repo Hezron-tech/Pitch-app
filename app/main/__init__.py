c16eef5a82f7cccaa449ea456b62c27f797fdf8c
Hezron-tech <kiprophez@gmail.com>
2022-03-06 13:58:21 +0300

    adding categories functions

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,12 +4,17 @@ from wtforms.validators import DataRequired
 
 class PitchForm(FlaskForm):
   title = StringField('Pitch title',validators=[DataRequired()])
-  category = SelectField("Choose Category",choices=[('business','business'),('sports','sports'),('politics','politics')])
+  category = SelectField("Choose Category",choices=[('sciences','sciences'),('tech','tech'),('tech','tech')])
   pitch_info = TextAreaField('Your Pitch',validators=[DataRequired()])
   submit = SubmitField('Submit')
+
+
+
 class CommentForm(FlaskForm):
   comment = TextAreaField('Your Comment',validators=[DataRequired()])
   submit = SubmitField('Submit')
+
+  
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
 from flask import render_template,request,redirect,url_for
-from flask import render_template
+from .forms import PitchForm,CommentForm, UpdateProfile
 from app import app
+from . import main
 from ..models import User,Pitch
-from flask_login import login_required
+from flask_login import login_required,current_user
 
 # Views
 @app.route('/')
@@ -15,4 +16,42 @@ def index():
     print(all_pitches)
     title='Pitch App'
     return render_template('index.html',title=title)
+
+@login_required
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        category = form.category.data
+        newPitch = form.pitch_info.data
+        #update pitch instance
+        new_pitch = Pitch(pitch_title=title,
+                          pitch_category=category,
+                          pitch_itself=newPitch,
+                          user=current_user)
+        #save pitch
+        new_pitch.save_pitch()
+        return redirect(url_for('.index'))
+    title = 'Add New pitch'
+    return render_template('pitches.html', title=title, pitchesform=form) 
+
+
+@main.route('/category/tech')
+def tech():
+    '''
+    view function to display sports pitches
+    '''
+    pitches=Pitch.get_pitches('tech')
+    tech_title ='tech Pitches'
+    return render_template('pitches/tech.html',title=tech_title,tech_pitch=pitches)
+
+
+@main.route('/category/sciences')
+def sciences():
+    '''
+    view function to display business pitches
+    '''
+    pitches=Pitch.get_pitches('sciences')
+    sciences_title='sciences Pitches'
+    return render_template('pitches/business.html',title=sciences_title, sciences_pitch=pitches)       
     
