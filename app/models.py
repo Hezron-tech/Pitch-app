bb9f2585f43adabb159713c8b0a2310691295c62
Hezron-tech <kiprophez@gmail.com>
2022-03-06 22:18:02 +0300

    adding getting comment method

--- app/models.py
+++ app/models.py
@@ -1,45 +1,46 @@
+from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
-from . import login_manager
 from flask_login import UserMixin
+from . import login_manager
 from datetime import datetime
 
-from . import db
+
+
+
+#...
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return User.query.get(int(user_id))  
+        
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
 
-class User(db.Model):
-
-    __tablename__="users"
-
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
-    username = db.Column(db.String(1000))
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    pitches = db.relationship("Pitch", backref="user", lazy="dynamic")
+    comment = db.relationship("Comments", backref="user", lazy="dynamic")
     bio = db.Column(db.String(255))
-    pitch = db.relationship("Pitch", backref="author", lazy="dynamic")
     profile_pic_path = db.Column(db.String())
-    pass_secure  = db.Column(db.String(255))
     password_hash = db.Column(db.String(255))
 
 
 
     @property
     def password(self):
-        raise AttributeError('You cannot read the password attribute')
+        raise AttributeError('You cannnot read the password attribute')
 
     @password.setter
     def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
 
     def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
         return f'User {self.username}'
-
-
 
 class Pitch(db.Model):
     __tablename__ = 'pitches'
@@ -48,11 +49,9 @@ class Pitch(db.Model):
     pitch_title = db.Column(db.String)
     pitch_category = db.Column(db.String)
     pitch_itself = db.Column(db.String)
-   
+    posted = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    
-
-
+    comment = db.relationship('Comments', backref='pitch', lazy="dynamic")
     def save_pitch(self):
         db.session.add(self)
         db.session.commit()
@@ -69,9 +68,6 @@ class Pitch(db.Model):
         Pitch.all_pitches.clear()
 
 
-def __repr__(self):
-        return f"Pitch ('{self.title}' , '{self.category}')"
-
 class Comments(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
@@ -86,26 +82,5 @@ class Comments(db.Model):
     def get_comments(self, id):
         comment = Comments.query.order_by(
             Comments.time_posted.desc()).filter_by(pitches_id=id).all()
-        return comment        
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-    
-
-    
-
-    
-
+        return comment
 
